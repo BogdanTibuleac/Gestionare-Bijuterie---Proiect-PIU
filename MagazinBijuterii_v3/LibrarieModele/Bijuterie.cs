d33030e3decbe4d99c0a29751baa3e627b2f16d6
Bogdan <127897204+BogdanTibuleac@users.noreply.github.com>
2023-03-30 14:25:47 +0300

    Add files via upload

---
+++ MagazinBijuterii_v3/LibrarieModele/Bijuterie.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace LibrarieModele
+{
+    public class Bijuterie
+    {
+        private const char SEPARATOR = '/';
+
+        private const int TIP = 0;
+        private const int MATERIAL = 1;
+        private const int PIETRE_PRETIOASE = 2;
+        private const int IN_STOC = 3;
+        private const int PRET = 4;
+
+        public string Tip { get; set; }
+        public string Material { get; set; }
+        public bool PietrePretioase { get; set; }
+        public bool InStoc { get; set; }
+        public int Pret { get; set; }
+
+
+        public Bijuterie() 
+        {
+            Tip =  string.Empty;
+            Material = string.Empty;
+            PietrePretioase = false;
+            InStoc = true;
+            Pret = 0;
+        }
+
+        public Bijuterie(string _tip, string _material, bool _pietrepretioase, bool _instoc, int _pret)
+        {
+            Tip = _tip;
+            Material = _material;
+            PietrePretioase= _pietrepretioase;
+            InStoc = _instoc;
+            Pret = _pret;
+        }
+
+        public Bijuterie(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR);
+            Tip = dateFisier[TIP];
+            Material = dateFisier[MATERIAL];
+            InStoc = Convert.ToBoolean(dateFisier[IN_STOC]);
+            PietrePretioase = Convert.ToBoolean(dateFisier[PIETRE_PRETIOASE]);
+            Pret = Convert.ToInt32(dateFisier[PRET]);
+        }
+        
+        public static Bijuterie CitesteDateTastatura()
+        {
+            Console.WriteLine("Dati tipul bijuteriei:");
+            string tip = Console.ReadLine();
+
+            Console.WriteLine("Dati materialul bijuteriei:");
+            string material = Console.ReadLine();
+
+            Console.WriteLine("Introduceti (true/false) daca bijuteria are pietre pretioase :");
+            bool pietrepretioase = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti (true/false) daca bijuteria este in stoc :");
+            bool instoc = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dati pretul bijuteriei:");
+            int pret = int.Parse(Console.ReadLine());
+
+            return new Bijuterie(tip, material, pietrepretioase, instoc, pret);
+        }
+        public string GetTip()
+        {
+            return Tip;
+        }
+
+        public string GetMaterial()
+        {
+            return Material;
+        }
+
+        public int GetPret()
+        {
+            return Pret;
+        }
+
+        public bool GetPietrePretioase()
+        {
+            return PietrePretioase;
+        }
+
+        public bool GetStoc()
+        {
+            return InStoc;
+        }
+        public void AfiseazaBijuterie()     
+        {
+            string string_afisare = string.Format("Bijuteria este de :\n" +
+                "Tip: {0} \tMaterial: {1} \tPietre pretioase {2} \tIn Stoc: {3} \tPret: {4}",
+                GetTip(),
+                GetMaterial(),
+                GetPietrePretioase(),
+                GetStoc(),
+                GetPret());
+            Console.WriteLine(string_afisare);
+        }
+        public string Info()
+        {
+            string info = string.Format("Tip: {0} \tMaterial: {1} \tPietre pretioase: {2} \tIn Stoc: {3} \tPret: {4}",
+                GetTip(),
+                GetMaterial(),
+                GetPietrePretioase(),
+                GetStoc(),
+                GetPret());
+            return info;
+        }
+        public string ConversieLaSir_PentruFisier() 
+        {
+            string string_fisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}",
+                SEPARATOR,
+                GetTip(),
+                GetMaterial(),
+                GetPietrePretioase(),
+                GetStoc(),
+                GetPret());
+            return string_fisier;
+        }
+    }
+}
+        
---
+++ MagazinBijuterii_v3/exercitiu/exercitiu/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] tablouScara = new string[26][];
+            char[] index = new char[26];
+            int[] sizeIndex = new int[26];
+            int i;
+            char c = 'A';
+            for (i = 0; i < 26; i++)
+            {
+                index[i] = c++;
+                sizeIndex[i] = 0;
+            }
+
+            if (args.Length == 0)
+            {
+
+                Console.Write("Linia de comanda nu contine argumente");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Numarul de argumente este: {0}", args.Length);
+                foreach (string param in args)
+                    for (i = 0; i < 26; i++)
+                        if (param[0] == index[i] || param[0] == index[i] + 32)
+                            sizeIndex[i]++;
+                for (i = 0; i < 26; i++)
+                {
+                    tablouScara[i] = new string[sizeIndex[i]];
+                    sizeIndex[i] = 0; 
+                }
+
+                foreach (string param in args)
+                    for (i = 0; i < 26; i++)
+                        if (param[0] == index[i] || param[0] == index[i] + 32)
+                            tablouScara[i][sizeIndex[i]++] += param;
+
+                for (i = 0; i < 26; i++)
+                {
+                    Console.Write("Cuvinte cu " + (char)(index[i] + 32) + "/" + index[i] + ":");
+                    for (int j = 0; j < sizeIndex[i]; j++)
+                        Console.Write(tablouScara[i][j] + " ");
+                    Console.WriteLine();
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
